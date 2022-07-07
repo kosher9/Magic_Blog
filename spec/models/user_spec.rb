6d89477900a82c2038465f1e462155acc75f9101
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-07 23:55:34 +0000

    Add rspec test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-  it 'should ' do
-    expect(User.create())
+  it 'is valid with valid attributes ' do
+    expect(User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                       bio: 'Teacher from Mexico.')).to be_valid
+  end
+
+  it 'is not valid with name empty ' do
+    expect(User.create(name: '', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                       bio: 'Teacher from Mexico.')).to_not be_valid
+  end
+
+  it 'is not valid with posts_counter nil' do
+    expect(User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                       bio: 'Teacher from Mexico.', posts_counter: nil)).to_not be_valid
+  end
+
+  it 'is not valid with posts_counter of type string' do
+    expect(User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                       bio: 'Teacher from Mexico.', posts_counter: 'nil')).to_not be_valid
   end
 end
