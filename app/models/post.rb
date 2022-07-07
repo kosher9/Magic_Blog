7fdc469cd339c79acd5c1593927a39d392ebc023
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-07 23:23:00 +0000

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ApplicationRecord
+
+  validates :title, length: { in: 1..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   after_save :update_post_counter
-
-  # validates :title, length: { in: 1..250 }
-  # validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_counter
     author.increment(:posts_counter)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+
+  validates :name, length: { minimum: 1 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
-
-  # validates :name, length: { minimum: 1 }
-  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
