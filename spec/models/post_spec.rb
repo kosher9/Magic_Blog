2ef14652148bbbd649e544f274e0ba8288783f6b
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-07 23:55:55 +0000

    Add rspec test for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @author = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Mexico.')
+  end
 
+  it 'is valid with valid attributes' do
+    expect(Post.create(author: @author, title: 'Hello', text: 'This is my first post')).to be_valid
+  end
+
+  it 'is not valid with title empty' do
+    expect(Post.create(author: @author, title: '', text: 'This is my first post')).to_not be_valid
+  end
+
+  it 'is not valid with title exceeding 250 characters ' do
+    title = 'r' * 251
+    expect(Post.create(author: @author, title:, text: 'This is my first post')).to_not be_valid
+  end
 
 end
