4a353430b803bfe017c9f8f572eb1645813ed51d
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-12 11:34:45 +0000

    Add html rendering to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    render html: '<div>Here is the list of all his posts</div>'.html_safe
+  end
+
+  def show
+    render html: '<div>Here is the post you are looking for</div>'.html_safe
+  end
 end
