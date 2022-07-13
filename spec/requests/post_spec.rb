c19651ab72019215b662183462836611c159ab5a
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-13 13:10:19 +0000

    Add test for post response body

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe Post, type: :request do
     it 'should render \'index\' template' do
       expect(response).to render_template('index')
     end
+
+    it 'should have the corresponding placeholder' do
+      expect(response.body).to include('Here is a list of all posts from that user')
+    end
   end
 
   describe 'GET #show' do
@@ -22,6 +26,10 @@ RSpec.describe Post, type: :request do
 
     it 'should render \'show\' template' do
       expect(response).to render_template('show')
+    end
+
+    it 'should have the corresponding placeholder' do
+      expect(response.body).to include('Here is a the post you are looking for')
     end
   end
 end
