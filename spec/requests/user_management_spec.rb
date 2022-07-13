a982e6d5e0c03deca830fe0f70f126b3a7662cc0
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-13 12:59:45 +0000

    Add test for user controller

---
+++ spec/requests/user_management_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+  describe 'GET #index' do
+    before(:example) { get users_path }
+
+    it 'should be a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render \'index\' template' do
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get user_path(1) }
+
+    it 'should be a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render \'show\' template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
