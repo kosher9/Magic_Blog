e4a85132a023efa36ea5bfa0b7000cb87f5a7ca2
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-15 15:51:51 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show] do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
+    end
   end
 
 
