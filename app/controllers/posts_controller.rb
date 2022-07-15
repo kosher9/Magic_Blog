03880d3b818261584595cfaf7a10473477affda0
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-15 23:58:04 +0000

    Fix redirection issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,31 @@ class PostsController < ApplicationController
   end
 
   def new
+    puts('life in new')
     @user = ApplicationController.new.current_user
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    add_post = @user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if add_post.save
+          flash[:success] = 'Post created successfully'
+          redirect_to user_posts_path
+        else
+          flash.now[:error] = 'Error: Post could not be created'
+          render :new, locals: { post: add_post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
