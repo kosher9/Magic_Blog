9f05e6e460957f2db693ed8ef6e13b6fb08e3e1c
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-16 03:28:48 +0000

    Fix linters -1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,5 +30,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text)
   end
-
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,8 @@
 class LikesController < ApplicationController
-
   def create
     @post = Post.find(params[:post_id])
     @user = ApplicationController.new.current_user
-    # @like = @user.likes.new
     add_like = Like.create(author: @user, post: @post)
-    # add_like.post = @post
-    # add_like.author = @user
     @post.save
     respond_to do |format|
       format.html do
@@ -17,8 +13,6 @@ class LikesController < ApplicationController
           render :new, locals: { like: add_like }
         end
       end
-      # redirect_to user_posts_url
     end
   end
-
 end
