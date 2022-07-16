01a46f6df7aaced9c2ba128c2918bdcbe04f5ba9
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-16 00:07:33 +0000

    Update comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,12 @@
 class CommentsController < ApplicationController
-  def new; end
+  def new
+    @user = ApplicationController.new.current_user
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    add_comment = @user.posts.new(post_params)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new]
-      resources :likes, only: %i[new]
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
     end
   end
 
