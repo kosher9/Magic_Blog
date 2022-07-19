a18ef6d415d6d9e3649685dc53d2172ff8433f8f
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-19 07:56:38 +0000

    Indented

--- spec/feature/user_spec.rb
+++ spec/feature/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe 'Testing user index page', type: :feature do
   describe 'GET index' do
     before(:each) do
@@ -6,13 +7,16 @@ RSpec.describe 'Testing user index page', type: :feature do
       @second_user = User.create(name: 'Sarah', photo: 'profile.jpg', bio: 'Web Developer.', posts_counter: 0)
       @second_user = User.create(name: 'Tobin', photo: 'profile.jpg', bio: 'Frontend Developer.', posts_counter: 0)
     end
-    scenario 'shows the right content' do
-      visit users_path
-      sleep(5)
-      expect(page).to have_content('Create new post')
-    end
+
     feature 'User Index' do
       background { visit users_path }
+
+      scenario 'shows the right content' do
+        visit users_path
+        sleep(5)
+        expect(page).to have_content('Create new post')
+      end
+
       scenario 'See all usernames' do
         expect(page).to have_content('Sarah')
         expect(page).to have_content('Tobin')
