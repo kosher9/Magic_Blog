4f8c28d82ed6f92021dfd5849b76366f281eb873
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-20 18:17:11 +0000

    Add auth page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def after_sign_in_path_for(resource_or_scope)
+  #   users_path
+  # end
+
+  # def after_sign_out_path_for(resource_or_scope)
+  #   redirect_to new_user_session_path
+  # end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = %i[name email password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'users#index'
-
+  # get(devise_session)
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
