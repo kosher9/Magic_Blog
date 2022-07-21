6941246425ecafecff3bc26c8458750277f6c8b3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-21 21:39:08 +0300

    Update checked and fixed found offences

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,10 @@ class Ability
   def initialize(user)
     # can :read, [Post, Comment], author: user
     return unless user.present?
+
     can :destroy, [Post, Comment]
     return unless user.role?
+
     can :destroy, [Post, Comment]
   end
 end
