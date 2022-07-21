c7e91cd8013b788162d4a4c0c28a5c289d8dec51
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-21 19:10:30 +0300

    Update included:   - Nullify forthe posts to user   - nulify for the likes to user   - nuify for the comments to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 1 }
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id, dependent: :nullify
+  has_many :likes, foreign_key: :author_id, dependent: :nullify
+  has_many :posts, foreign_key: :author_id, dependent: :nullify
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
