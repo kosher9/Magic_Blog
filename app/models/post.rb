6497a6c90158055257333c9c2afce447117ed9dc
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-21 19:12:29 +0300

    Update included:   - Update nulify for comments for a post   - nulify for a likes for a pos

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :nullify
+  has_many :comments, dependent: :nullify
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   after_save :update_post_counter
