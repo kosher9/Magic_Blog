586d40daed9dc828ede2b8e48c0efd61607f0ff1
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-21 14:18:57 +0000

    Set authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :destroy, Post, author: user
+    can :read, [Post, Comment], public: true
     return unless user.present?
-    can :destroy, :Post
-    return unless user.admin?
-    can :destroy, :Post
+    can :destroy, [Post, Comment]
+    return unless user.role?
+    can :destroy, [Post, Comment]
   end
 end
--- db/migrate/20220721105916_add_admin_to_users.rb
+++ db/migrate/20220721105916_add_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddAdminToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :admin, :string
+    add_column :users, :role, :string
   end
 end
