06ec4748bc7cdd3940e9e44408f59f0d98c161ce
Cédric Kossi <lionel.kossi@gmail.com>
2022-07-22 01:07:50 +0000

    Check admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
 
       user ||= User.new # guest user (not logged in)
 
-      if user.admin?
+      if user.role == 'admin'
         can :manage, :all
       else
         can :manage, Post, author: user
